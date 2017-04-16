3cba9ec80383358f78922cda2dfdad6c40df3e4f
qizhi <707573223@qq.com>
2017-04-16 17:09:11 +0800

    53. Maximum Subarray

--- src/main/java/Q53MaximumSubarray/MaximumSubarray.java
+++ src/main/java/Q53MaximumSubarray/MaximumSubarray.java
@@ -5,7 +5,47 @@ package Q53MaximumSubarray;
  */
 public class MaximumSubarray {
 	public static void main(String[] args) {
-		System.out.println("test2");
-		System.out.println("hello");
+		int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+//		for (int i=0;i<nums.length;i++){
+//			System.out.println(nums[i]);
+//		}
+
+		method2(nums);
+	}
+
+	private static void method2(int[] nums) {
+
+		int ans = Integer.MIN_VALUE;
+		int sum = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (sum < 0)
+				sum = 0;
+			if (sum + nums[i] > ans) {
+				ans = sum + nums[i];
+			}
+			sum += nums[i];
+		}
+		System.out.println(ans);
+//		return ans;
+	}
+
+	private static void method1(int[] nums) {
+		int sumArr[] = new int[nums.length + 1];
+		sumArr[0] = 0;
+		for (int i = 0; i < nums.length; i++) {
+			sumArr[i + 1] = sumArr[i] + nums[i];
+		}
+		int ans = Integer.MIN_VALUE;
+		int minPre = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (sumArr[i] < minPre) {
+				minPre = sumArr[i];
+			}
+			if (sumArr[i + 1] - minPre > ans) {
+				ans = sumArr[i + 1] - minPre;
+			}
+		}
+		System.out.println(ans);
+//		return ans;
 	}
 }
